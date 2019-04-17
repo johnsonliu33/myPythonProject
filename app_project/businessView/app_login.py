9a4bc951c9e32692db4337648a124eeeed78fc5a
song <79446928@qq.com>
2019-04-17 23:56:22 +0800

    update desired_caps.yaml appPackage

--- app_project/businessView/app_login.py
+++ app_project/businessView/app_login.py
@@ -1,8 +1,8 @@
-from jiandan_project.common.app_common import Common
+from app_project.common.app_common import Common
 from selenium.webdriver.common.by import By
-from jiandan_project.baseView.desired_caps import appium_desired
+from app_project.baseView.desired_caps import appium_desired
 import logging.config
-
+from time import sleep
 CON_LOG = "../config/log.conf"
 logging.config.fileConfig(CON_LOG)
 logs = logging.getLogger()
@@ -14,13 +14,16 @@ class LoginView(Common):
     login_btn = (By.ID, "com.jiandan.mobilelesson:id/login_btn")
 
     def login_action(self, username, password):
-        self.common_fun()
+        self.is_upgrade()
+        self.get_screen_shot("login")
 
         logs.info("login username: %s" % username)
         self.driver.find_element(*self.username_type).send_keys(username)
         logs.info("login password: %s" % password)
         self.driver.find_element(*self.password_type).send_keys(password)
         self.driver.find_element(*self.login_btn).click()
+        sleep(5)
+        self.swipe_up()
 
 
 if __name__ == '__main__':
