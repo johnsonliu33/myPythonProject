e04e1b20dc9db0155a0e45325eca81302980e7ac
song <79446928@qq.com>
2019-05-08 00:22:27 +0800

    python启动多个appium

--- app_project/baseView/multi_appium.py
+++ app_project/baseView/multi_appium.py
@@ -4,7 +4,11 @@ from time import ctime
 
 def appium_start(host, port):
     bootstrap_port = str(port + 1)
-    cmd = "start /b appium -a " + host + " -p " + str(port) + " -bp " + str(bootstrap_port)  # /b 不启动命令窗口
+    # 启动多个appium服务时，不要安装客户端
+    # npm install -g cnpm --registry=https://registry.npm.taobao.org
+    # cnpm install -g appium
+    cmd = "start /b appium -a " + host + " -p " + str(port) + " -bp " + str(bootstrap_port)
+    # 说明：/b 不打开命令窗口; bp端口（ --bootstrap-port）是appium和设备之间通信的端口，如果不指定到时无法操作多台设备运行脚本。
     print("{} at {}".format(cmd, ctime()))
     subprocess.Popen(cmd, shell=True, stdout=open("./appium_log/" + str(port) + ".log", "a"), stderr=subprocess.STDOUT)
 
