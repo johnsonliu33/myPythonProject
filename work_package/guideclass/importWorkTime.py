befbb4a93ed63af3bd3b2e22bbbb1923df1c3b5c
clientpub <songqianqian@jiandan100.cn>
2019-05-09 17:02:43 +0800

    批量插入班会时间

--- work_package/guideclass/importWorkTime.py
+++ work_package/guideclass/importWorkTime.py
@@ -6,12 +6,13 @@ import datetime
 def get_mongo():
     uri = "mongodb://guideclass:zaq1xsw2@172.16.0.166:27017/guideclass_ceshi"
     client = MongoClient(uri)
-    collection = client["guideclass_ceshi"]["guiderworktimes"]
-    return collection
+    dataBase = client["guideclass_ceshi"]
+    return dataBase
 
 
 def WorkTime():
-    collect = get_mongo()
+    dataBase = get_mongo()
+    collection = dataBase.guiderworktimes
     with open("worktime.csv", "r") as file:
         data_list = csv.reader(file)
         for row in data_list:
@@ -29,9 +30,41 @@ def WorkTime():
                 "time": row[2],
                 "updatedAt": datetime.datetime.utcnow()
             }
-            collect.insert_one(records)
+            collection.insert_one(records)
+
+
+def MeetTime():
+    dataBase = get_mongo()
+    collection = dataBase.meetingtimes
+    with open("meettime.csv", "r") as file:
+        data_list = csv.reader(file)
+        for row in data_list:
+            print(row)
+            item = row[1].split(" ")
+            days = item[0].split("/")
+            times = item[1].split(":")
+            year = int(days[0])
+            month = int(days[1])
+            day = int(days[2])
+            hour = int(times[0])
+            minute = int(times[1])
+            second = int(times[2])
+            records = {
+                "isValid": "true",
+                "guider": row[0],
+                "classTime": datetime.datetime(year, month, day, hour, minute, second),
+                "duration": row[2],
+                "textDesc": row[3],
+                "id": row[4],
+                "createdAt": datetime.datetime.utcnow(),
+                "updatedAt": datetime.datetime.utcnow(),
+                "__v": 0
+            }
+            collection.insert_one(records)
+
 
 
 if __name__ == '__main__':
     print(datetime.datetime.utcnow())
-    WorkTime()
+    # WorkTime()
+    MeetTime()
