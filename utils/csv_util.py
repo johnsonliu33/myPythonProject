452b0d017eccd932d2777aa528c8c07f15089ef0
song <79446928@qq.com>
2019-05-22 07:22:33 +0800

    csv操作

--- utils/csv_util.py
+++ utils/csv_util.py
@@ -4,7 +4,7 @@ import csv
 def get_csv_data(file_name, line):
     with open(file_name, 'r', encoding='utf-8-sig')as file:
         read = csv.reader(file)
-        for index, row in enumerate(read):  # index为行号 # row是该行内容.  # line表示坐标从line开始计数,默认index从0开始
+        for index, row in enumerate(read):  # index为行号 # row是该行内容.  # enumerate(read,star=10)表示坐标从10开始计数,默认index从0开始
             if index == line:
                 return row
 
