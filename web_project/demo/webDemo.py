848da5bde3a75fab3ba49aca48ab021a8cec3967
song <79446928@qq.com>
2019-05-25 08:00:56 +0800

    web_project

--- web_project/demo/webDemo.py
+++ web_project/demo/webDemo.py
@@ -2,18 +2,23 @@
 from selenium import webdriver
 from time import sleep
 
-driver=webdriver.Firefox()
-driver.get("http://www.51zxw.com")
+driver = webdriver.Firefox()
+driver.get("http://www.baidu.com")
 print(driver.title)
 sleep(3)
 driver.maximize_window()
-sleep(2)
 driver.refresh()
-sleep(2)
-driver.set_window_size(400,800)
-sleep(2)
+driver.set_window_size(400, 800)
 driver.back()
-sleep(2)
+
+# 获取元素 8个
+driver.find_elements_by_id()
+driver.find_element_by_name()
+driver.find_element_by_class_name()
+driver.find_elements_by_xpath()
+driver.find_element_by_link_text()
+driver.find_element_by_partial_link_text()
+driver.find_element_by_tag_name()
+driver.find_element_by_css_selector()
 
 driver.quit()
-sleep(2)
