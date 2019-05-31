75302fe6483eb42dbc44f0d5b9a9ab7afa211299
song <79446928@qq.com>
2019-06-01 07:38:23 +0800

    06-01

--- web_project/src/util/send_email.py
+++ web_project/src/util/send_email.py
@@ -1,59 +1,43 @@
 # 发送带附件的邮件
-from email import encoders
 from email.mime.text import MIMEText
 from email.utils import formatdate
 from email.mime.multipart import MIMEMultipart
-from email.mime.base import MIMEBase
 from email.header import Header
 import smtplib
 import os.path
 
 
-def send_email(report_name):
-    sender = "songqianqian@jiandan100.cn"
-
-    # 多个收件人用逗号隔开
-    receives = ["779446928@qq.com", "songqianqian@jiandan100.cn"]
-
-    cc = ["1220688565@qq.com", "1220688565@qq.com"]
-    file_name = report_name
-
-    server = smtplib.SMTP("mail.jiandan100.cn")
-    server.login("songqianqian@jiandan100.cn", "Mm9842")  # 仅smtp服务器需要验证时
-
+def send_email(latest_report):
     # 构造MIMEMultipart对象做为根容器
     main_msg = MIMEMultipart()
+    # 发送邮件主题和内容
+    subject = "按不出的法国"
+    content = open(latest_report, "rb").read()
+    main_msg.attach(MIMEText(content, "html", "utf-8"))
+    # 构造附件内容
+    send_file = open(latest_report, "rb").read()
+    text = MIMEText(send_file, "base64", "utf-8")
+    text["Content-Type"] = "application/octet-stream"
+    text["Content-Disposition"] = "attachment;filename='report.html'"
 
-    # 构造MIMEText对象做为邮件显示内容并附加到根容器
-    with open(file_name, "rb") as file:
-        text_msg = file.read()
-    main_msg = MIMEText(text_msg, "html", "utf-8")
-    # main_msg.attach(text_msg)
-
-    # 构造MIMEBase对象做为文件附件内容并附加到根容器
-    contype = "application/octet-stream"
-    maintype, subtype = contype.split("/", 1)
-
-    # 读入文件内容并格式化
-    data = open(file_name, "rb")
-    file_msg = MIMEBase(maintype, subtype)
-    file_msg.set_payload(data.read())
-    data.close()
-    encoders.encode_base64(file_msg)
-
-    # 设置附件头
-    basename = os.path.basename(file_name)
-    # 解决中文附件名乱码问题
-    file_msg.add_header("Content-Disposition", "attachment", filename=("gbk", "", basename))
-    main_msg.attach(file_msg)
+    sender = "songqianqian@jiandan100.cn"
+    receives = ["779446928@qq.com", "songqianqian@jiandan100.cn"]
+    cc = ["1220688565@qq.com", "1220688565@qq.com"]
+    smtpserver = "mail.jiandan100.cn"
+    server = smtplib.SMTP(smtpserver)
+    # 向服务器标识用户身份
+    server.helo(smtpserver)
+    # 服务器返回结果确认
+    server.ehlo(smtpserver)
+    server.login("songqianqian@jiandan100.cn", "Mm9842")  # 仅smtp服务器需要验证时
 
     # 设置根容器属性
     main_msg["From"] = sender
     main_msg["To"] = ",".join(receives)
-
     main_msg["Cc"] = ",".join(cc)
-    main_msg["Subject"] = Header("邮件", "utf-8")
+    main_msg["Subject"] = Header(subject, "utf-8")
     main_msg["Date"] = formatdate()
+    main_msg.attach(text)
 
     # 得到格式化后的完整文本
     fullText = main_msg.as_string()
@@ -73,5 +57,5 @@ if __name__ == "__main__":
     report_dir = "../../report"
     lists = os.listdir(report_dir)
     lists.sort(key=lambda x: os.path.getatime(report_dir))  # 按文件时间排序
-    report_path = os.path.join(report_dir, lists[-1])
-    send_email(report_path)
+    latest_report = os.path.join(report_dir, lists[-1])
+    send_email(latest_report)
