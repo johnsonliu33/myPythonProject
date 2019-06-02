3f5419da549cf3bc6b4d716c23944c19760f6f75
song <79446928@qq.com>
2019-06-02 16:11:05 +0800

    dump_mongo

--- work_package/back_mongodb/dump_mongo.py
+++ work_package/back_mongodb/dump_mongo.py
@@ -1,31 +1,39 @@
-# -*- coding:utf-8 -*- 
+# -*- coding:utf-8 -*-
 
 import time
 import os
-
-DBUSER = "guideclass"  # 用户
-DBPASS = "zaq1xsw2"  # 密码
-IP = "172.16.0.166"  # 主机
-# now = time.strftime("%Y-%m-%d")
-DATA_DIR = "./back_data"  # 目录
-DATA_BASE = "guideclass_ceshi2"  # 库名
-PATH_DUMP = "/usr/local/mongodb/bin/mongodump"  # 命令路径
-BACKITEMS = ["%s -h %s:27017 -u %s -p %s -d %s -o %s" % (PATH_DUMP, IP, DBUSER, DBPASS, DATA_BASE, DATA_DIR),
-             "echo $? "]
-mk_dir = "mkdir back_data"
+import sys
 
 
-def dump_data():
+def dump():
+    DATA_BASE = sys.argv[1]  # 库名
+    DBUSER = "guideclass"  # 用户
+    DBPASS = "zaq1xsw2"  # 密码
+    IP = "172.16.0.166"  # 主机
+    now = time.strftime("%Y-%m-%d")
+    DATA_DIR = "./back_data"  # 目录
+    PATH_DUMP = "/usr/local/mongodb/bin/mongodump"  # 命令路径
+    print("-----------", sys.argv[1])
+    BACKITEMS = ["%s -h %s:27017 -u %s -p %s -d %s -o %s" % (PATH_DUMP, IP, DBUSER, DBPASS, DATA_BASE, DATA_DIR),
+                 "date"]
+    mk_dir = "mkdir back_data"
     if not os.path.exists("back_data"):
         os.system(mk_dir)
     try:
         for item in BACKITEMS:
             print("============" + item + "============")
-            os.system(item)
-            print("succes")
-    except RuntimeError as r:
-        print("------" + r + "------")
+            print(os.system(item))
+    except RuntimeError as e:
+        print("------" + e + "------")
 
 
-if __name__ == "__main__":
-    dump_data()
+def main():
+    usage = " [-]usage: dump_mongo.py <DATA_BASE> \n     eg : python dump_mongo.py guideclass_ceshi "
+    if len(sys.argv) < 2:
+        print(usage)
+    else:
+        dump()
+
+
+if __name__ == '__main__':
+    main()
