952a7c93c06a28addd0320a8a9cef72d7720c005
song <79446928@qq.com>
2019-06-12 08:26:36 +0800

    django

--- django_restful/django_restful/urls.py
+++ django_restful/django_restful/urls.py
@@ -18,13 +18,16 @@ from django.urls import path
 from django.conf.urls import include
 from rest_framework import routers
 from api import views
+from rest_framework.schemas import get_schema_view
+from rest_framework_swagger.renderers import SwaggerUIRenderer, OpenAPIRenderer
 
 router = routers.DefaultRouter()
 router.register("users", views.UserViewSet)
 router.register("groups", views.GroupViewSet)
-
+schema_view = get_schema_view(title="API", renderer_classes=[OpenAPIRenderer, SwaggerUIRenderer])
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('',include(router.urls)),
-    path('api-auth/',include('rest_framework.urls',namespace='rest_framework'))
+    path('', include(router.urls)),
+    path('api-auth/', include('rest_framework.urls', namespace='rest_framework')),
+    path('docs/', schema_view, name="docs")
 ]
