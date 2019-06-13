678033ea5e493387d4aa985cecc481f0e599366a
song <79446928@qq.com>
2019-06-13 08:13:15 +0800

    django+unittest

--- django_restful/api/tests.py
+++ django_restful/api/tests.py
@@ -1,3 +1,32 @@
 from django.test import TestCase
+import requests
+import unittest
+
 
 # Create your tests here.
+
+class UserTest(unittest.TestCase):
+    def setUp(self):
+        self.base_url = "http://127.0.0.1:8000/users/"
+        self.auth = ("root", "123456")
+
+    def test_get_user(self):
+        resp = requests.get(url=self.base_url + "1/", auth=self.auth)
+        result = resp.json()
+        print(result)
+        self.assertEqual(result["username"], "root")
+        self.assertEqual(result["email"], "779446928@qq.com")
+
+    def test_add_user(self):
+        from_data = {"username": "xiaoming4", "email": "123@qq.com"}
+        resp = requests.post(url=self.base_url, data=from_data, auth=self.auth)
+        result = resp.json()
+        print(result)
+        self.assertEqual(result["username"], "xiaoming4")
+
+    def tearDown(self):
+        pass
+
+
+if __name__ == '__main__':
+    unittest.main()
