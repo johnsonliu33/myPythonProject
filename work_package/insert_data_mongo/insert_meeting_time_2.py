fc243c7b60be0d878ebbfa6d75906fe9d35d6369
宋谦谦 <songqianqian@jiandan100.cn>
2019-06-30 02:08:55 +0800

    插入班会添加年级和学管师

--- work_package/insert_data_mongo/insert_meeting_time_2.py
+++ work_package/insert_data_mongo/insert_meeting_time_2.py
@@ -1,11 +1,12 @@
 import xlrd
 import datetime
-from pymongo import MongoClient,errors
+from pymongo import MongoClient, errors
 
 
 def get_mongo():
     DATABASE = "guideclass_ceshi2"
-    uri = "mongodb://guideclass:zaq1xsw2@172.16.0.166:27017/{}".format(DATABASE)
+    uri = "mongodb://guideclass:zaq1xsw2@172.16.0.166:27017/{}".format(
+        DATABASE)
     client = MongoClient(uri)
     collection = client[DATABASE]
     return collection
@@ -16,8 +17,8 @@ def get_seqidgens_collect(collect):
 
 
 def get_meetingtimes_collect(collect):
-    return collect["meetingtimes"]
-    # return collect["test"]
+    # return collect["meetingtimes"]
+    return collect["test"]
 
 
 def get_id(collect, user, init=123):
@@ -28,7 +29,8 @@ def get_id(collect, user, init=123):
         "updatedAt": datetime.datetime.utcnow()}}, new=True, upsert=True, setDefaultsOnInsert=True)
     if getRes and (getRes["seq"] < init):
         getRes = collect.find_one_and_update({"_id": user},
-                                             {"$inc": {"seq": init}, "$set": {"updatedAt": datetime.datetime.utcnow()}},
+                                             {"$inc": {"seq": init}, "$set": {
+                                                 "updatedAt": datetime.datetime.utcnow()}},
                                              new=True, upsert=True, setDefaultsOnInsert=True)
     return getRes["seq"]
 
@@ -37,7 +39,35 @@ def create_id(sid):
     return "mt_" + str(sid)
 
 
-def utc_dateTime(book_datemode, date_value, time_value):
+def guide_type_dict(var):
+    return {
+        "初一": "cz1",
+        "初二": "cz2",
+        "初三": "cz3",
+        "六年级": "6",
+        "七年级": "7",
+        "八年级": "8",
+        "九年级": "9",
+        "高一": "gz1",
+        "高二": "gz2",
+        "高三": "gz3",
+        "初中": "cz1,cz2,cz3,6,7,8,8",
+        "高中": "gz1,gz2,gz3",
+    }.get(var, "guide is not find")
+
+def get_guide_type_set(guide_type_list):
+    if guide_type_list is None:
+        return "guide_type_list is none"
+    gts=[]
+    for item_list in guide_type_list:
+        item=guide_type_dict(item_list).split(",")
+        gts.extend(item)
+    gts=list(set(gts))
+    gts.sort()
+    return gts
+
+
+def utc_date_time(book_datemode, date_value, time_value):
     if isinstance(date_value, str):
         date1 = date_value.split("/")
         year = date1[0]
@@ -57,14 +87,20 @@ def utc_dateTime(book_datemode, date_value, time_value):
         time2 = xlrd.xldate_as_tuple(date_value, book_datemode)
         hour = time2[3:][0]
         minute = time2[3:][1]
-    date_time = datetime.datetime(int(year), int(month), int(day), int(hour), int(minute), 0)
+    date_time = datetime.datetime(
+        int(year),
+        int(month),
+        int(day),
+        int(hour),
+        int(minute),
+        0)
     utc_date_time = date_time + datetime.timedelta(hours=-8)
     return utc_date_time
 
 
 # exec()用来执行储存在字符串或文件中的Python语句,支持Python代码的动态执行
 def exec(meetingtimes_collect, seqidgens_collect):
-    book = xlrd.open_workbook("班会时间安排 -衡水荣春波.xlsx")
+    book = xlrd.open_workbook("demo-直播班会时间安排.xlsx")
     sheet = book.sheet_by_index(0)
     colnum = sheet.nrows
     valid_num = 0
@@ -75,18 +111,20 @@ def exec(meetingtimes_collect, seqidgens_collect):
         if grade == "" or grade == "年级":
             continue
         date_value = sheet.cell_value(i, 1)
+        guide_type=sheet.cell_value(i, 2)
+        guide_type_list=guide_type.split("、")
+        guide_type_set=get_guide_type_set(guide_type_list)
         time_value = sheet.cell_value(i, 3)
-        guide_username= sheet._cell_values(i,4)
+        guide_username = sheet.cell_value(i, 4)
         book_datemode = book.datemode
-        utc_meeting_date = utc_dateTime(book_datemode, date_value, time_value)
-        gradeType = "cz" if grade == "初中" else "gz"
+        utc_meeting_date = utc_date_time(book_datemode, date_value, time_value)
         create_id(get_id(seqidgens_collect, "meetingTimeId", 1000000)),
         records = {
             "isValid": True,
             "classTime": utc_meeting_date,
             "duration": 30,
-            "gradeType": gradeType,
-            "guider":guide_username,
+            "gradeType": guide_type_set,
+            "guider": guide_username,
             "createdAt": datetime.datetime.utcnow(),
             "updatedAt": datetime.datetime.utcnow()
         }
@@ -100,10 +138,10 @@ def exec(meetingtimes_collect, seqidgens_collect):
     ############################################
     ############################################
 
-    print("===================")
+    print("="*10)
     print("=有效数据为: " + str(valid_num) + " 条=")
     print("=插入数据为: " + str(insert_num) + " 条=")
-    print("===================")
+    print("="*10)
 
 
 if __name__ == "__main__":
