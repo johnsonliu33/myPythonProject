19f47e0b775e688ae48ff32fd42c2e14f6b7c707
宋谦谦 <songqianqian@jiandan100.cn>
2019-07-08 16:43:57 +0800

    导入班会时间

--- work_package/insert_data_mongo/insert_meeting_time.py
+++ work_package/insert_data_mongo/insert_meeting_time.py
@@ -35,8 +35,7 @@ def get_meetingtimes_collect(collect):
 
 def get_id(collect, user, init=123):
     # $inc:   ({"_id": "meetingTimeId"},{$inc:{"seq":1}})   -- 找到id是meetingTimeId的数据，把seq参数加1
-    # $set:   ({"_id": "meetingTimeId"},{$set:{"updatedAt":datetime.datetime.utcnow()}})
-    #           -- 找到id是meetingTimeId的数据，更新updatedAt时间为当前时间
+    # $set:   ({"_id": "meetingTimeId"},{$set:{"updatedAt":datetime.datetime.utcnow()}}) -- 找到id是meetingTimeId的数据，更新updatedAt时间为当前时间
     # upsert=true时，匹配不到数据会插入一条新数据
     getRes = collect.find_one_and_update({"_id": user}, {"$inc": {"seq": 1}, "$set": {
         "updatedAt": datetime.datetime.utcnow()}}, new=True, upsert=True, setDefaultsOnInsert=True)
@@ -120,6 +119,7 @@ def utc_date_time(book_datemode, date_value, time_value):
 
 # exec()用来执行储存在字符串或文件中的Python语句,支持Python代码的动态执行
 def exec(meetingtimes_collect, seqidgens_collect, xlsx_name):
+    # book = xlrd.open_workbook("demo-直播班会时间安排.xlsx")
     book = xlrd.open_workbook(xlsx_name)
     sheet = book.sheet_by_index(0)
     col_num = sheet.nrows
