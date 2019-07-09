57237fc2350567677ce2e06a42e4c6d04e107c2b
宋谦谦 <songqianqian@jiandan100.cn>
2019-07-09 18:23:47 +0800

    导入班会时间

--- work_package/guideclass/update_usePass.py
+++ work_package/guideclass/update_usePass.py
@@ -3,7 +3,7 @@ from pymongo import MongoClient
 import pymysql
 
 
-def mysqlUtil(sqlStr, username):
+def mysqlUtil(sqlStr):
     host = "192.168.20.156"
     port = 3306
     user = "test_user"
@@ -40,16 +40,16 @@ def insert_user(username):
                 (`id`, `username`, `userpwd`, `realname`, `sex`, `email`, `schoolname`, `provincename`, `districtname`, `subdistrictname`, `grade`, `gradetype`, `classname`, `tel`, `mobile`, `isdelete`, `registertime`, `lastmodifytime`, `filepathid`, `registerip`, `usertype`, `knowtype`, `othertype`, `qqno`, `isipuser`, `easycash`, `userfigure`, `subjectclassify`, `subject`, `sourcetype`, `ipcode`, `ischargeaccount`, `isinvited`, `isdetailed`, `areacode`, `studyrecordemail`, `nickname`, `facedata`, `mobilebind`)
                 VALUES
                 (null, '%s', 'VMtLmys=', '', 2, '', '房山区青龙湖中学', '北京', '北京', '房山区', '02-2016', 0, '', '', '18888888888', 0, '2018-11-04 03:00:50', '2018-11-04 03:18:16', NULL, '111.199.221.70', 0, 0, '', '19790221', 0, 0, 4, 0, '', 0, '0000', 1, 1, 1, '', '', '', '', 1);""" % username
-    mysqlUtil(sql_one, username)
+    mysqlUtil(sql_one)
 
 
 def update_userPass(username):
     sql_two = "update W_UserBaseInfo set userpwd='VMtLmys=', realname='简而优'  where username ='%s';" % username
-    mysqlUtil(sql_two, username)
+    mysqlUtil(sql_two)
 
 
 
-def get_user():
+def upset_user():
     db = mongo_datebase()
     cellec_users = db.users
     data_users = cellec_users.find()
@@ -62,7 +62,7 @@ def get_user():
     print("有效用户：%s 个"% num)
 
 
-def update_username():
+def update_realName():
     database=mongo_datebase()
     collect = database["users"]
     user_list = collect.find()
@@ -74,7 +74,7 @@ def update_username():
                 collect.update_one({"realName": user_name_old}, {
                                    "$set": {"realName": user_name_new}})
 
-def update_studentname():
+def update_studentRealName():
     database=mongo_datebase()
     collect = database["students"]
     user_list = collect.find()
@@ -88,4 +88,4 @@ def update_studentname():
 
 
 if __name__ == '__main__':
-    update_studentname()
+    update_studentRealName()
