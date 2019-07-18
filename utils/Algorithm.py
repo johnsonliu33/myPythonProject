4e1d84bb5f2f6d21ba82ae5aaa4d547244d883d7
宋谦谦 <songqianqian@jiandan100.cn>
2019-07-18 17:39:36 +0800

    suanfa

--- utils/Algorithm.py
+++ utils/Algorithm.py
@@ -4,9 +4,8 @@
 # 一、冒泡排序：
 # 每轮依次比较相邻两个数的大小，后面比前面小则交换
 def bubble_sort(temp_list):
-    n = len(temp_list)
-    for i in range(n):
-        for j in range(n - i - 1):
+    for i in range(len(temp_list) - 1):
+        for j in range(len(temp_list) - 1):
             if temp_list[j] > temp_list[j + 1]:
                 temp_list[j], temp_list[j + 1] = temp_list[j + 1], temp_list[j]
     print(temp_list)
@@ -16,22 +15,30 @@ def bubble_sort(temp_list):
 # 拿第一个数与后面数相比较，如果比后面的数大则交换
 # 拿第二个数与后面的数比较，如果比后面的数大则交换
 # 直到比较到倒数第二个数，最后一个数不用比较
-# 两个数比较可以用中间变量替换或者位运算
-# 利用位运算时需注意，如果两个数相等则不能使用位运算
-
-
+def choice_sort(temp_list):
+    for i in range(len(temp_list) - 1):
+        for j in range(i + 1, len(temp_list)):
+            if temp_list[i] > temp_list[j]:
+                temp_list[i], temp_list[j] = temp_list[j], temp_list[i]
+    print(temp_list)
 
 
 # 三、快速排序思路：
 # 选择第一个数为基点，右边 j 开始查找比基点小的数停止，再从左边 i 查找比基点数大的数停住
-# 调换 i、j 对应的数后执行步骤 2，知道 i 和 j 相遇，此时 i == j
-# 调换 i 对应的数和基点数，将源数列一分为 2 后分别重复步骤 2、3、4，知道结束
-# 注意使用位运算调换时必须判断交换两个数不能相等，否则第二个数为 0
+# 调换 i、j 对应的数后执行步骤 2，直到 i 和 j 相遇，此时 i == j
+# 调换 i 对应的数和基点数，将源数列一分为 2 后分别重复步骤 2、3、4，直到结束
+def quick_sort(temp_list):
+    temp_list.sort()
+    print(temp_list)
 
 
 if __name__ == '__main__':
-    temp_list = [3, 7, 1, 5, 4, 2, 8, 0]
-    bubble_sort(temp_list)
+    temp_list1 = [6, 3, 7, 1, 9, 5, 4, 2, 8]
+    bubble_sort(temp_list1)
+    temp_list2 = [6, 3, 7, 1, 9, 5, 4, 2, 8]
+    choice_sort(temp_list2)
+    temp_list3 = [6, 3, 7, 1, 9, 5, 4, 2, 8]
+    quick_sort(temp_list3)
 
 # 总结：
 # 1、选择排序不稳定，时间复杂度为 O(n2)
