8a7361c9be45d10172aaacba0e3ca97beb4015c0
宋谦谦 <songqianqian@jiandan100.cn>
2019-07-22 17:09:28 +0800

    check student

--- work_package/guideclass/checkGrade.py
+++ work_package/guideclass/checkGrade.py
@@ -19,7 +19,8 @@ def get_students():
 
     query = {}
     query["$or"] = [{"$and": [{"enrollYear": {"$lte": "2016"}}, {"gradeType": "cz3"}]},
-                    {"$and": [{"enrollYear": {"$lte": "2016"}}, {"gradeType": "gz3"}]},
+                    {"$and": [{"enrollYear": {"$lte": "2016"}},
+                              {"gradeType": "gz3"}]},
                     {"$and": [{"enrollYear": {"$lte": "2015"}}, {"gradeType": "cz4"}]}]
     # query["gradeType"] = "cz3"
     projection = {}
@@ -35,7 +36,7 @@ def get_students():
 def get_guider(err_students):
     db = get_mongodb()
     c_guiderels = db.guiderels
-    err_stu_list=[]
+    err_stu_list = []
     for err_student in err_students:
         query = {}
         query["student"] = err_student["username"]
@@ -48,37 +49,41 @@ def get_guider(err_students):
         projection["guider"] = "$guider"
         projection["_id"] = 0
 
-        sort = [ ("guider", -1) ]
+        sort = [("guider", -1)]
 
-        cursor = c_guiderels.find(query, projection = projection, sort = sort,limit = 1)
-        if "dingzhi" not in err_student["username"] and "jianeryou" not in err_student["username"]and "xued" not in err_student["username"]:
+        cursor = c_guiderels.find(
+            query, projection=projection, sort=sort, limit=1)
+        if "dingzhi" not in err_student["username"] and "jianeryou" not in err_student[
+                "username"]and "xued" not in err_student["username"]:
             for guider in cursor:
-                es=err_student["username"],err_student["gradeType"],err_student["enrollYear"],guider["guider"]
+                es = err_student["username"], err_student["gradeType"], err_student["enrollYear"], guider["guider"]
                 err_stu_list.append(es)
     return err_stu_list
+
+
 def subject_type(var):
     return {
-        "1":"语文",
+        "1": "语文",
         "2": "数学",
         "3": "英语",
         "4": "物理",
         "5": "化学",
-        "6":"生物",
-        "79":"史政",
-        "8":"地理"
+        "6": "生物",
+        "79": "史政",
+        "8": "地理"
     }.get(var, "学科不存在！")
+
 
 def get_exam():
     db = get_mongodb()
     c_guidereserves = db.guidereserves
     query = {}
     query["time"] = {
-        "$lte": datetime.datetime.strptime("2019-07-22 08:00:00.000000", "%Y-%m-%d %H:%M:%S.%f").replace(tzinfo = FixedOffset(480, "+0800")),
-        "$gte": datetime.datetime.strptime("2019-07-20 08:00:00.000000", "%Y-%m-%d %H:%M:%S.%f").replace(tzinfo = FixedOffset(480, "+0800"))
+        "$lte": datetime.datetime.strptime("2019-07-22 08:00:00.000000", "%Y-%m-%d %H:%M:%S.%f").replace(tzinfo=FixedOffset(480, "+0800")),
+        "$gte": datetime.datetime.strptime("2019-07-20 08:00:00.000000", "%Y-%m-%d %H:%M:%S.%f").replace(tzinfo=FixedOffset(480, "+0800"))
     }
 
     query["firstTime"] = False
-    query["isValid"] = True
 
     projection = {}
     projection["student"] = "$student"
@@ -87,19 +92,30 @@ def get_exam():
     projection["time"] = "$time"
     projection["_id"] = 0
 
-    cursor = c_guidereserves.find(query, projection = projection, skip = 1007, limit = 1000)
+    cursor = c_guidereserves.find(query, projection=projection)
 
-    stu_list=[]
+    stu_list = []
     for temp in cursor:
-        if "dingzhi" not in temp["student"] and "jianeryou" not in temp["student"]:
-            d=[temp["student"],subject_type(temp["subject"]),temp["guider"],datetime.datetime.strftime(temp["time"], "%Y-%m-%d %H:%M:%S")]
+        if "dingzhi" not in temp["student"] and "jianeryou" not in temp["student"] and "https" not in temp["student"]:
+            d = [
+                temp["student"],
+                subject_type(
+                    temp["subject"]),
+                temp["guider"],
+                datetime.datetime.strftime(
+                    temp["time"],
+                    "%Y-%m-%d"),
+                datetime.datetime.strftime(
+                    temp["time"],
+                    "%H:%M:%S")]
             stu_list.append(d)
     return stu_list
 
 
 if __name__ == '__main__':
-    err_students=get_students()
-    err_stu_list=get_guider(err_students)
-    excel_util.write_excel("errorstudent.xlsx",err_stu_list)
-    stu_list=get_exam()
-    excel_util.write_excel("check_student.xlsx",stu_list)
+    # err_students = get_students()
+    # err_stu_list = get_guider(err_students)
+    # excel_util.write_excel("errorstudent.xlsx", err_stu_list)
+    stu_list = get_exam()
+    str_time = datetime.datetime.now().strftime("%Y-%m-%d")
+    excel_util.write_excel(str_time+"_exam_student.xlsx", stu_list)
