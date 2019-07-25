a279c07003108c26cd737cbe9c7f21a30c2c72ca
宋谦谦 <songqianqian@jiandan100.cn>
2019-07-25 13:52:25 +0800

    0725

--- utils/timed_task.py
+++ utils/timed_task.py
@@ -3,8 +3,9 @@
 import time
 import datetime
 
-interval = 1 * 60  #乘以60秒
-end_time = datetime.datetime(2019, 7, 25, 13, 20, 0)
-while datetime.datetime.now() < end_time:
-    print("定时任务..." + datetime.datetime.strftime("%Y-%m-%d %H:%M:%S"))
+interval = 1 * 60
+end_time = datetime.datetime(2019, 7, 25, 13, 32, 0)
+while datetime.datetime.now() <= end_time:
+    print("定时任务..." + datetime.datetime.now().strftime("%Y-%m-%d %H:%M:%S"))
     time.sleep(interval)
+print("定时任务结束！！！")
