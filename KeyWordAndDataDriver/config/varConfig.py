cfc7964d502d2925a133a7a44ffdcc6a804c3c2a
song <79446928@qq.com>
2019-07-30 00:20:02 +0800

    keyword and data driver

--- KeyWordAndDataDriver/config/varConfig.py
+++ KeyWordAndDataDriver/config/varConfig.py
@@ -2,9 +2,11 @@
 import os
 
 # 获取当前文件所在目录的父目录的绝对路径
-parentDirPath= os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+parentDirPath = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
 ieDriverFilePath = os.path.join(parentDirPath, "driver", "IEDriverServer.exe")
 chromeDriverFilePath = os.path.join(parentDirPath, "driver", "chromedriver.exe")
 firefoxDriverFilePath = os.path.join(parentDirPath, "driver", "geckodriver.exe")
 
+# 异常图片存放目录
+screenPicturesDir = os.path.join(parentDirPath, "exceptionIctures")
