767afafa85df7e63e50e2a4416db4840a3f30947
song <79446928@qq.com>
2019-07-30 00:22:14 +0800

    keyword and data driver

--- KeyWordAndDataDriver/util/dirAndTime.py
+++ KeyWordAndDataDriver/util/dirAndTime.py
@@ -1,27 +1,30 @@
 # -*- coding:utf-8 -*-
 
 import os
-import datetime
+from datetime import date, datetime
 from config.varConfig import screenPicturesDir
 
 
 # 获取当前的日期
 def getCurrentDate():
-    currentDate = datetime.date.today()
+    currentDate = date.today()
     return currentDate
+
 
 # 获取当前的时间
 def getCurrentTime():
-    timeStr= datetime.datetime.now()
-    nowTime =timeStr.strftime("%H - %M - %S - %f")
+    timeStr = datetime.now()
+    nowTime = timeStr.strftime("%H - %M - %S - %f")
     return nowTime
+
 
 # 创建截图存放的目录
 def ceateCurrentDateDie():
-    dirName= os.path.join(screenPicturesDir,str(getCurrentDate()))
+    dirName = os.path.join(screenPicturesDir, str(getCurrentDate()))
     if not os.path.exists(dirName):
         os.mkdir(dirName)
     return dirName
+
 
 if __name__ == '__main__':
     print(getCurrentDate())
